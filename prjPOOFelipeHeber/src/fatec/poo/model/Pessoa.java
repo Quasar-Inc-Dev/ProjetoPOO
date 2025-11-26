4f06e9708b3dea0dd5ee94e4f7f3a28e500c281d
mhebe <mheber36@gmail.com>
2025-11-26 12:04:23 -0200

    feature: Criação do método validarCpf

--- prjPOOFelipeHeber/src/fatec/poo/model/Pessoa.java
+++ prjPOOFelipeHeber/src/fatec/poo/model/Pessoa.java
@@ -43,4 +43,46 @@ public class Pessoa {
     public String getNome() {
         return nome;
     }
+    
+    public static boolean validarCpf(String cpf) {
+       int primeiroDigito = 0;
+       int segundoDigito = 0;
+        
+       //Verificação dos digitos iguais usando Regex, recebendo um digito(d) inteiro(0-9) e repete(l) por 10 vezes
+        if (cpf.matches("(\\d\\1{10}")) {
+            return false;
+        }
+        
+        for(int i = 0; i < 9; i++){
+            int peso = 1;
+            int digito = Integer.parseInt(cpf.substring(i, i++));
+            primeiroDigito += digito * (i * peso);
+            peso++;
+        }
+        
+        primeiroDigito = primeiroDigito%11;
+        if (primeiroDigito == 10){
+            primeiroDigito = 0;
+        }
+        
+        for(int i = 0; i < 9; i++){
+            int peso = 11;
+            int digito = Integer.parseInt(cpf.substring(i, i++));
+            segundoDigito += digito * (i * peso);
+            peso--;
+        }
+        
+        segundoDigito = (segundoDigito*10)%11;
+        if (segundoDigito == 10) {
+            segundoDigito = 0;
+        }
+        
+        int primeiroDigitoFinal = Integer.parseInt(cpf.substring(9, 10));
+        int segundoDigitoFinal = Integer.parseInt(cpf.substring(10, 11));
+        
+        if (primeiroDigito == primeiroDigitoFinal || segundoDigito == segundoDigitoFinal){
+            return true;
+        }
+        return false;
+    }
 }
